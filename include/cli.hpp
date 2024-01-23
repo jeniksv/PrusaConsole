90c0620589eb13b5bea874bc74cfd3e0849de2d7
Jan Svojanovský <jan.svojanovsky@prusa3d.cz>
2024-01-23 17:40:52 +0100

    haha

---
+++ include/cli.hpp
@@ -0,0 +1,50 @@
+#ifndef CONSOLE_READER_H_
+#define CONSOLE_READER_H_
+
+#include <string>
+
+#include "tab_completion.hpp"
+#include "history.hpp"
+#include "key_actions.hpp"
+#include "printer.hpp"
+#include "parser.hpp"
+
+#include "cpp-terminal/cursor.hpp"
+#include "cpp-terminal/exception.hpp"
+#include "cpp-terminal/input.hpp"
+#include "cpp-terminal/iostream.hpp"
+#include "cpp-terminal/key.hpp"
+#include "cpp-terminal/options.hpp"
+#include "cpp-terminal/screen.hpp"
+#include "cpp-terminal/terminal.hpp"
+#include "cpp-terminal/tty.hpp"
+#include "cpp-terminal/version.hpp"
+
+
+class cli{
+public:
+	cli();
+
+	void mainloop();
+private:
+	void show_prompt();
+
+	void reset_state();
+
+	void clear_line(const std::string&);
+
+	void write_line(const std::string&);
+
+	void process_command();
+
+	bool _running;
+	std::string _current_line;
+
+	history _history;
+	tab_completion _tab;
+	printer _printer;
+	command_parser _parser;
+	key_action_factory _key_action_factory;
+};
+
+#endif
---
+++ src/cli.cpp
@@ -0,0 +1,73 @@
+#include "cli.hpp"
+
+cli::cli() : _running(true), _current_line(), _history(".prusa_console_history"), _tab(), _printer(), _parser(), _key_action_factory(_running, _history, _tab, _printer, _parser) {
+	Term::terminal.setOptions(Term::Option::NoClearScreen, Term::Option::NoSignalKeys, Term::Option::Cursor, Term::Option::Raw);
+	Term::Screen term_size{Term::screen_size()};
+	Term::Cursor cursor{Term::cursor_position()};
+}
+
+
+void cli::show_prompt(){
+	Term::cout << "> " << std::flush;
+}
+
+
+void cli::clear_line(const std::string& s){
+	for(int i=0; i < s.length(); ++i){
+		Term::cout << "\b \b";
+	}
+}
+
+
+void cli::write_line(const std::string& s){
+	Term::cout << s << std::flush;
+}
+
+
+void cli::reset_state(){
+	show_prompt();
+	_history.reset();
+	_current_line = "";
+}
+
+
+void cli::process_command(){
+	_parser.process(_current_line);
+
+	if(!_parser.get_name().has_value() || !_printer.valid_command(_parser.get_name().value())){
+		reset_state();
+		return;
+	}
+
+	auto& dbus_request = _printer.get_action(_parser.get_name().value());
+	auto result = dbus_request->execute(_parser.get_arguments());
+
+	// TODO pochytat vsechno chyby z commandu
+
+	_running = result == command_result::EXIT;
+	reset_state();
+}
+
+
+void cli::mainloop(){
+	show_prompt();
+	
+	while(_running){
+		Term::Event event = Term::read_event();
+
+		if(event.type() != Term::Event::Type::Key){
+			continue;
+		}
+
+		Term::Key key(event);
+
+		clear_line(_current_line);
+		auto action = _key_action_factory.get_action(key);
+		auto action_result = action->execute(_current_line);
+		write_line(_current_line);
+	
+		if(action_result == key_action_result::COMMAND_READY){
+			process_command();
+		}
+	}
+}
